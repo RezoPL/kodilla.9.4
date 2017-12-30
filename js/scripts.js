1584dfaca305e50ac78c26e590bed1382fd1d2c2
Przemek Jurkiewicz <hankusekbb@gmail.com>
2017-12-30 18:25:00 +0100

    add scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function drawTree(height) {
+	for (var i = 0; i < height; i++) {
+		var star = "";
+		for (var j = 0; j <height - i; j++) {
+			star += " ";
+		}
+		for (var j = 0; j <2 * i + 1; j++) {
+			star += '*';
+		}
+		console.log(star);
+	}
+}
+drawTree(7);
