a6dd92a3df345a1a050406ba950d8e80baa26cd5
Przemek Jurkiewicz <hankusekbb@gmail.com>
2018-01-04 20:34:42 +0100

    scripts.js edit

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 function drawTree(height) {
-	for (var i = 0; i <height; i++) {
+	for (var i = 0; i < height; i++) {
 		var star = "";
-		for (var j = 0; j <height - i; j++) {
+		for (var j = 0; j < height - i; j++) {
 			star += " ";
 		}
-		for (var j = 0; j <2 * i + 1; j++) {
+		for (var j = 0; j < 2 * i + 1; j++) {
 			star += '*';
 		}
 		console.log(star);
